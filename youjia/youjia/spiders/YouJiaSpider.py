8390f866704541d4a50b84b94f3a7e6c8ffa7d10
dugang <timor.du@hotmail.com>
2018-05-30 11:52:06 +0800

    优化代码

--- youjia/youjia/spiders/YouJiaSpider.py
+++ youjia/youjia/spiders/YouJiaSpider.py
@@ -9,16 +9,15 @@ class YouJiaSpider(scrapy.Spider):
     start_urls = ['http://youjia.chemcp.com/']
 
     def parse(self, response):
-        trs = response.xpath('//*[@class="chem_3"]/div[1]/div[2]/table/*')
-        for index in range(len(trs)):
+        for tr in response.xpath('//*[@class="chem_3"]/div[1]/div[2]/table/*'):
             try:
                 item = PriceItem()
-                item['area'] = trs[index].xpath('td[1]/a/text()')[0].extract()
-                item['_89'] = trs[index].xpath('td[2]/text()')[0].extract()
-                item['_92'] = trs[index].xpath('td[3]/text()')[0].extract()
-                item['_95'] = trs[index].xpath('td[4]/text()')[0].extract()
-                item['_0'] = trs[index].xpath('td[5]/text()')[0].extract()
-                item['update_time'] = trs[index].xpath('td[6]/text()')[0].extract()
+                item['area'] = tr.xpath('td[1]/a/text()')[0].extract()
+                item['_89'] = tr.xpath('td[2]/text()')[0].extract()
+                item['_92'] = tr.xpath('td[3]/text()')[0].extract()
+                item['_95'] = tr.xpath('td[4]/text()')[0].extract()
+                item['_0'] = tr.xpath('td[5]/text()')[0].extract()
+                item['update_time'] = tr.xpath('td[6]/text()')[0].extract()
                 yield item
             except IndexError:
                 pass
